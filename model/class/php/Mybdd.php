b47f9baabc2a923f37cad8c1ccbe8a3410119f24
Luvumbu <luvumbu.n@gmail.com>
2020-11-23 23:09:51 +0100

    amellioration de lalgo ok

--- model/class/php/Mybdd.php
+++ model/class/php/Mybdd.php
@@ -1,113 +1,191 @@
 <?php
-class Fruit {
-  public $servername ;
-  public $username ;
-  public $password ;
-  public $dbnam;
-  public $row_name= array();
-  public $row_value=array();
-
-  
-
-  public $exe_ok=false;
-  function __construct($servername,$username,$password,$dbname) {
-    $this->servername = $servername;
-    $this->username = $username;
-    $this->password = $password;
-    $this->dbname =   $dbname;
-  }
+class Bdd_methode {
  
-  function set_row_name($set_row_name){
-    array_push($this->row_name,$set_row_name);
-  }
-  function get_row_name($integer){
-    return $this->row_name[$integer];
-  }
-  function count_row_name(){
-    return count($this->row_name);
-  }
 
-  function set_row_value($set_row_value){
-    array_push($this->row_value,$set_row_value);
-  }
-  function count_row_value(){
-    return count($this->row_value);
-  }
-  function insertion(){
-      
-    $conn2 = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-    // Check connection
-    if ($conn2->connect_error) {
-      die("Connection failed: " . $conn2->connect_error);
-    }
-    
-          // $sql = "INSERT INTO test (nom)
-          // VALUES ('John')";
-          
-          // if ($conn2->query($sql) === TRUE) {
-          //   echo "New record created successfully";
-          // } else {
-          //   echo "Error: " . $sql . "<br>" . $conn2->error;
-          // }
-          
-          // $conn2->close();
-          
-  }
-
-  function number_row_value($number){
+private $servername ;
+private $username ;
+private $password ;
+private $dbname ;
  
- //return $number;
+ 
+ private $check=false;
+ private $row_array_number= array();
+ private $row_array_name= array();
+ private $row_array_result= array();
+ private $row_array_combine= array();
+ 
 
-
- if($this->count_row_name()>$number){
-  return $this->row_value[$number];
+ function set_row_array_number($set){
+  array_push($this->row_array_number,$set);
  }
- else{
-   return "Erreur set_row_name trop eleve";
- } 
-  }
- 
-  function set_select($select){
-$this->select = $select;
-  }
-  function get_select(){
-    return $this->select;
-      }
-  function exe(){
-    $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+ function get_number_row_array_number($set){ 
+  return $this->row_array_number[$set];
+ }
+ function count_row_array_number(){  
+  return count($this->row_array_number);
+ }
 
-$sql = $this->get_select();
-$result = $conn->query($sql);
+ function set_row_array_name($set){
+  array_push($this->row_array_name,$set);
+ }
+ function get_number_row_array_name($set){ 
+  return $this->row_array_name[$set];
+ }
+ function count_row_array_name(){  
+  return count($this->row_array_name);
+ }
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {     
-    for($i = 0 ; $i<$this->count_row_name();$i++){
-     $this->set_row_value($row[$this->get_row_name($i)]);
-     
-    }    
-  }
-} else {
+ function set_row_array_result($set){
+  array_push($this->row_array_result,$set);
+ }
+ function get_number_row_array_result($set){ 
+  return $this->row_array_result[$set];
+ }
+
+
+ function set_row_array_combine(){
+  $this->row_array_combine=array_combine($this->row_array_name,$this->row_array_result); 
   
+ }
 
-  $this->set_row_value("X");
-  // insertion des informations  
-  echo $this-> count_row_name();
-  $this->insertion();
+ function get_row_array_combine($number){
+  return  $this->row_array_combine[$number];
  
-  // fin de linsertion des informations
 }
-$conn->close();
-  } 
-} 
-// $apple = new Fruit("localhost","root","root","bokonzi_all");
-// $apple->set_select('SELECT * FROM `club` WHERE `club_nom`="alors"');
-// $apple->set_row_name("club_nom");// demande des information dans le arroow 
-// $apple->set_row_name("club_region");// demande des information dans le arroow 
-// $apple->exe(); // execution du programme 
-// echo $apple->number_row_value(0);  
+
+
+ 
+ 
+ 
+ 
+
+  function __construct($servername,$username,$password,$dbname) {
+    $this->servername= $servername;
+    $this->username= $username;
+    $this->password= $password;
+    $this->dbname= $dbname;
+  }
+  function get_servername(){
+    return $this->servername;
+  }
+  function get_username(){
+    return $this->username;
+  }
+  function get_password(){
+    return $this->password;
+  }
+  function get_dbname(){
+    return $this->dbname;
+  }
+
+  function set_ckeck_array(){
+    
+  }
+
+  function set_row(){
+
+  }
+  function set_servername($servername){
+     $this->servername=$servername;
+  }
+  function set_username($username){
+     $this->username=$username;
+  }
+  function set_password($password){
+     $this->password=$password;
+  }
+  function set_dbname($dbname){
+     $this->dbname=$dbname;
+  }
+  function set_sql($set_sql){
+    $this->set_sql = $set_sql;
+  }
+  function get_sql(){    
+    return $this->set_sql ; 
+  }
+  function set_bdd_check($bdd_check){
+    $this->bdd_check=$bdd_check;
+  }
+
+ 
+  function check_exe(){
+    
+    try {
+ 
+      $conn = new PDO("mysql:host=$this->servername;dbname=".$this->dbname."", $this->username, $this->password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      //echo "Connected successfully";      
+     $this->check=true;
+      $this->alors=9;
+    
+    
+      $this->conn= new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+    } catch(PDOException $e) {
+     // echo "Connection failed: " . $e->getMessage();
+    }
+  }
+  function execute(){
+
+// verification
+
+
+
+// finde la verification
+ $this->check_exe(); // test si il ya pas d'erreur
+ 
+ 
+if($this->check==true){
+  $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+  $sql = $this->get_sql();
+  $result = $conn->query($sql);
+  
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {   //   echo "id: " . $row["club_nom"];
+      
+      
+       
+      for($i=0;$i<$this->count_row_array_number();$i++){
+  
+        $this->set_row_array_name($this->get_number_row_array_number($i));
+       
+        $this->set_row_array_result($row[$this->get_number_row_array_number($i)]);
+        
+      }
+      $this->count_row_array_number();
+    }
+    $this->set_row_array_combine();
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
+}
+else{
+  echo "FALSE";
+}
+
+
+  }
+ 
+}
+
+$apple = new Bdd_methode("localhost","root","root","bokonzi_all");
+$apple->set_sql("SELECT * FROM `club` WHERE 1");
+
+$apple->set_row_array_number("club_nom");
+$apple->set_row_array_number("club_region");
+
+ 
+$apple->execute();  
+
+echo $apple->get_row_array_combine("club_region");
+//echo $apple->get_number_row_array_number(0);
+//echo $apple->count_row_array_number();
+ 
 ?>
--- model/class/php/config.php
+++ model/class/php/config.php
@@ -27,7 +27,10 @@ include("connexion.php");
                     $conn = new PDO("mysql:host=$servername;dbname=".$dbname, $username, $password);
                     // set the PDO error mode to exception
                     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    echo "Connected successfully";
+                   
+                   
+                   // execution du code ok 
+                   // echo "Connected successfully";
                   } catch(PDOException $e) {
 
                     echo "Connection failed: " . $e->getMessage();
